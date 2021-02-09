0fe0dd5e458244cf89da9341e9f1a0d2a711aeef
Bryan Rodriguez <bryanrr_julio_@hotmail.com>
2021-02-09 14:47:05 -0600

    Core structure of our Spring project

---
+++ src/main/java/com/autoservicio/puntoventa/PuntoventaApplication.java
@@ -0,0 +1,13 @@
+package com.autoservicio.puntoventa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuntoventaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuntoventaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/autoservicio/puntoventa/PuntoventaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoservicio.puntoventa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuntoventaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
