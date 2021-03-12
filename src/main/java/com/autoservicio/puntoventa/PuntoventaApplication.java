152158a915b2dd77e07c955d9f4925cc709d7e37
Bryan Rodriguez <bryanrr_julio_@hotmail.com>
2021-03-12 12:01:28 -0600

    Add CORS conifguration

--- src/main/java/com/autoservicio/puntoventa/PuntoventaApplication.java
+++ src/main/java/com/autoservicio/puntoventa/PuntoventaApplication.java
@@ -2,6 +2,9 @@ package com.autoservicio.puntoventa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PuntoventaApplication {
@@ -9,5 +12,14 @@ public class PuntoventaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PuntoventaApplication.class, args);
 	}
-
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("POST,GET,OPTIONS").allowedOriginPatterns("*");
+			}
+		};
+	}
 }
