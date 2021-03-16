87917a58d1a9764bcdc1eec6da66c0adbec54beb
Bryan Rodriguez <bryanrr_julio_@hotmail.com>
2021-03-16 11:30:33 -0600

    Update CORS mechanism
    Cors mechanism specified as a bean at root class wasn't working.
    Instead it was added a Cors filter for supoorting CORS request.

--- src/main/java/com/autoservicio/puntoventa/PuntoventaApplication.java
+++ src/main/java/com/autoservicio/puntoventa/PuntoventaApplication.java
@@ -12,14 +12,4 @@ public class PuntoventaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PuntoventaApplication.class, args);
 	}
-	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedMethods("POST,GET,OPTIONS").allowedOriginPatterns("*");
-			}
-		};
-	}
 }
